fb11b33bfb11254a962b1f0001723361a54a9ca4
Jhon Soto <jhon.soto@aflore.co>
2024-09-26 20:08:03 -0500

    feat(api): set up FastAPI and documentation routes.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from app.controllers.v1 import order_controller
 
+app = FastAPI(
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_url="/api/openapi.json",
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.title = "Beers Bar Api"
+app.version = "1.0"
+
+app.include_router(order_controller.router, prefix="/api/v1")
