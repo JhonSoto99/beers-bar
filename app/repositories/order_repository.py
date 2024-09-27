3fa2596676a56e8d9296282eacbc68cdaa960578
Jhon Soto <jhon.soto@aflore.co>
2024-09-26 19:34:29 -0500

    feat(order-repository): Add orders repository.

---
+++ app/repositories/order_repository.py
@@ -0,0 +1,42 @@
+from typing import Dict
+
+from app.models.order import Order
+from data.order_data import mock_order_data
+
+
+class OrderRepository:
+    """
+    Repository class for handling order data.
+
+    This class is responsible for loading and retrieving order information.
+    In this case, it uses mock data to simulate the database.
+    """
+
+    def __init__(self):
+        """
+        Initializes the OrderRepository.
+
+        Loads the order data from the provided mock data.
+        """
+        self.order: Order = self.load_order(mock_order_data)
+
+    def load_order(self, order_data: Dict) -> Order:
+        """
+        Loads an Order object from a dictionary of order data.
+
+        Args:
+            order_data (Dict): A dictionary representing the order data.
+
+        Returns:
+            Order: An instance of the Order model with the loaded data.
+        """
+        return Order(**order_data)
+
+    def get_order(self) -> Order:
+        """
+        Retrieves the current order.
+
+        Returns:
+            Order: The current loaded order.
+        """
+        return self.order
