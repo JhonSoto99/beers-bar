008342ea4613c6140da7b52bac334b194f425e63
Jhon Soto <jhon.soto@aflore.co>
2024-09-26 19:14:37 -0500

    feat(data): add mock orders data and stock data.

---
+++ data/order_data.py
@@ -0,0 +1,28 @@
+mock_order_data = {
+    "created": "2024-09-10 12:00:00",
+    "paid": False,
+    "subtotal": 0,
+    "taxes": 0,
+    "discounts": 0,
+    "items": [],
+    "rounds": [
+        {
+            "created": "2024-09-10 12:00:30",
+            "items": [
+                {"name": "Corona", "quantity": 2},
+                {"name": "Club Colombia", "quantity": 1},
+            ],
+        },
+        {
+            "created": "2024-09-10 12:20:31",
+            "items": [
+                {"name": "Club Colombia", "quantity": 1},
+                {"name": "Quilmes", "quantity": 2},
+            ],
+        },
+        {
+            "created": "2024-09-10 12:43:21",
+            "items": [{"name": "Quilmes", "quantity": 3}],
+        },
+    ],
+}
---
+++ data/stock_data.py
@@ -0,0 +1,8 @@
+mock_stock_data = {
+    "last_updated": "2024-09-10 12:00:00",
+    "beers": [
+        {"name": "Corona", "price": 115, "quantity": 2},
+        {"name": "Quilmes", "price": 120, "quantity": 0},
+        {"name": "Club Colombia", "price": 110, "quantity": 3},
+    ],
+}
