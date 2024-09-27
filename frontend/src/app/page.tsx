284e11c5fb446c0bbe6abce6347cc25184fefafd
Jhon Soto <jhon.soto@aflore.co>
2024-09-27 14:47:58 -0500

    refactor: Refactor code.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-        <Link href="/order">
-            <h1>Ir a ver la orden</h1>
-        </Link>
+    <main className="flex flex-col items-center justify-center h-screen text-center">
+      <h1 className="text-3xl font-bold mb-4">¡Welcome!</h1>
+      <Link href="/order" className="text-blue-500 hover:underline">
+        Go to view your order
+      </Link>
     </main>
   );
 }
