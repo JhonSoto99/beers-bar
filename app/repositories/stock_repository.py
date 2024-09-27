872efee6dae7aa6f3d015b146799bcb632765068
Jhon Soto <jhon.soto@aflore.co>
2024-09-26 19:38:31 -0500

    feat(stock-repository): Add stock repository.

---
+++ app/repositories/stock_repository.py
@@ -0,0 +1,42 @@
+from typing import Dict
+
+from app.models.stock import Stock
+from data.stock_data import mock_stock_data
+
+
+class StockRepository:
+    """
+    Repository class for handling stock data.
+
+    This class is responsible for loading and retrieving stock information.
+    In this case, it uses mock data to simulate a database.
+    """
+
+    def __init__(self):
+        """
+        Initializes the StockRepository.
+
+        Loads the stock data from the provided mock data.
+        """
+        self.stock = self.load_stock(mock_stock_data)
+
+    def load_stock(self, stock_data: Dict) -> Stock:
+        """
+        Loads a Stock object from a dictionary of stock data.
+
+        Args:
+            stock_data (Dict): A dictionary representing the stock data.
+
+        Returns:
+            Stock: An instance of the Stock model with the loaded data.
+        """
+        return Stock(**stock_data)
+
+    def get_stock(self) -> Stock:
+        """
+        Retrieves the current stock.
+
+        Returns:
+            Stock: The current loaded stock.
+        """
+        return self.stock
